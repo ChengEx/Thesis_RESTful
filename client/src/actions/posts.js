4f1d27124b4588ea24a9a40dbf55a1d510c15bf5
ChengEx <frf2isgood@gmail.com>
2021-11-08 23:56:10 +0800

    11/8-2

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -3,8 +3,17 @@ import * as api from '../api';
 export const getPosts = () => async(dispatch) => {
     try{    
         const { data } = await api.fetchPosts();
-        dispatch({ type:'FETCH_ALL', payload: data});
+        dispatch({ type:'FETCH_ALL', payload: data });
     }catch(err){
         console.log(err.message)
+    }
+}
+
+export const createPost = (post) => async(dispatch) => {
+    try{    
+        const { data } = await api.createPost(post);
+        dispatch({ type:'CREATE', payload: data });
+    }catch(err){
+        console.log(err);
     }
 }
--- server/index.js
+++ server/index.js
@@ -18,6 +18,6 @@ const CONNEVTION_URL = "mongodb+srv://denoriaaa:SQ3No7zNIIwTmSBI@cluster0.hnuu7.
 const PORT =  process.env.PORT || 5000;
 
 mongoose.connect(CONNEVTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
-    .then(()=> app.listen(PORT,()=>console.log(`Server unning om port: ${PORT}`)))
+    .then(()=> app.listen(PORT,()=>console.log(`Server runing on port: ${PORT}`)))
     .catch((error)=>console.log(error.message));
 
