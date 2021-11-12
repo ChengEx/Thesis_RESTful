b01d631d1f8f93607738ff1ffe7a25719823030b
ChengEx <frf2isgood@gmail.com>
2021-11-12 17:58:46 +0800

    11/12-1

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,23 @@
-import React,{ useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-
-
-import { getPosts } from  './actions/posts';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import chat from './images/chat.png';
-import useStyles from './styles';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import Auth from './components/Auth/Auth';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
 
 const App=()=>{
-    const [ currentId, setCurrentId ] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(()=>{
-        dispatch(getPosts());
-    },[currentId, dispatch]);
+    
     return (
-        <Container maxidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Social Media</Typography>
-                <img className={classes.image} src={chat} alt="chat" height="60" />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId}/>
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </Grow>
-        </Container>
+        <BrowserRouter>
+            <Container maxidth="lg">
+                <Navbar/>
+                <Routes>
+                    <Route path="/" exact element={<Home />} />
+                    <Route path="/auth" exact element={<Auth/>} />
+                </Routes>
+            </Container>
+        </BrowserRouter>
+        
     )
 }
 
